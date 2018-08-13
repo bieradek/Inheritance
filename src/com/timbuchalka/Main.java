e4d909315085609800e51984dbc9efa1e87564c1
bieradek <radoslaw.biedrzycki@syncron.com>
2018-08-14 00:04:45 +0200

    Initial commit

---
+++ src/com/timbuchalka/Main.java
@@ -0,0 +1,10 @@
+package com.timbuchalka;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Dog burek = new Dog("Burek", 5, 15, 10, 60);
+
+    }
+}
