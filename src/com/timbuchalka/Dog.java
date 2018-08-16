4832c4983480a05398f57b4934ab24db46d601f2
bieradek <radoslaw.biedrzycki@syncron.com>
2018-08-16 18:11:06 +0200

    Initializing default constructor and avoiding super call redundance

--- src/com/timbuchalka/Dog.java
+++ src/com/timbuchalka/Dog.java
@@ -1,7 +1,20 @@
 package com.timbuchalka;
 
 public class Dog extends Animal {
-    public Dog(String name, int brain, int body, int size, int weight) {
-        super(name, brain, body, size, weight);
+
+    private int eyes;
+    private int legs;
+    private int tail;
+    private int teeth;
+    private String coat;
+
+    public Dog(String name, int size, int weight, int eyes, int legs, int tail, int teeth, String coat) {
+        // in order to avoid redundance we can remove brain and body and hard-code it in super call
+        super(name, 1, 1, size, weight);
+        this.eyes = eyes;
+        this.legs = legs;
+        this.tail = tail;
+        this.teeth = teeth;
+        this.coat = coat;
     }
 }
